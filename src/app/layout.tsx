44869513f908f78b998ed6ac33aa1a48fa746a18
guntar <guntar@me.com>
2025-01-05 05:56:33 +0700

    Update After Changing Machine to Windows

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -62,9 +62,9 @@ export default function RootLayout({
                   <Home className='w-5 h-5 mr-2' />
                   <span>Home</span>
                 </Link>
-                <Link href='/profil' className='flex items-center p-2 rounded-lg hover:bg-gray-700'>
+                <Link href='/bbs' className='flex items-center p-2 rounded-lg hover:bg-gray-700'>
                   <UserCog className='w-5 h-5 mr-2' />
-                  <span>Profil</span>
+                  <span>Board</span>
                 </Link>
                 <Link href='/aktivitas' className='flex items-center p-2 rounded-lg hover:bg-gray-700 mt-2'>
                   <Activity className='w-5 h-5 mr-2' />
@@ -73,6 +73,10 @@ export default function RootLayout({
                 <Link href='/leaderboard' className='flex items-center p-2 rounded-lg hover:bg-gray-700 mt-2'>
                   <Trophy className='w-5 h-5 mr-2' />
                   <span>Leaderboard</span>
+                </Link>
+                <Link href='/profil' className='flex items-center p-2 rounded-lg hover:bg-gray-700'>
+                  <UserCog className='w-5 h-5 mr-2' />
+                  <span>Profil</span>
                 </Link>
                 <Link href='/kelola-user' className='flex items-center p-2 rounded-lg hover:bg-gray-700 mt-2'>
                   <Users className='w-5 h-5 mr-2' />
